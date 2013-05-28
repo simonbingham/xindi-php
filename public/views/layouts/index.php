8eced8eba05a642d9f9287cea82cf5cfa083017f
Simon Bingham <me@simonbingham.me.uk>
2013-05-28 19:49:39 +0100

    Removed links to favicons located in root directory.

--- public/views/layouts/index.php
+++ public/views/layouts/index.php
@@ -34,14 +34,6 @@
 			</script>
 			<script src="public/assets/js/outbound-link-tracking.js"></script>
 		<?php } ?>
-
-		<link rel="shortcut icon" href="favicon.ico">
-		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="apple-touch-icon-144x144-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="apple-touch-icon-114x114-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="apple-touch-icon-72x72-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" sizes="57x57" href="apple-touch-icon-57x57-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" href="apple-touch-icon-precomposed.png">			
-		<link rel="apple-touch-icon" href="apple-touch-icon.png">		
 	</head>
 	
 	<body>
